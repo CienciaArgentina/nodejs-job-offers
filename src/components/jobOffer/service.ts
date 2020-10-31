161efbc07fc408586e448c372415e13f60074bc5
Lucas Francisco Lopez <lucaslopezf@gmail.com>
2020-10-31 18:55:35 -0300

    updat ebackedn commons

--- src/components/jobOffer/service.ts
+++ src/components/jobOffer/service.ts
@@ -1,25 +1,22 @@
-import { isNullOrUndefined } from 'util';
-import { HTTP404Error, HTTP400Error } from 'ciencia-argentina-backend-commons';
+import { notFoundError } from 'ciencia-argentina-backend-commons';
 import { findJobById } from './repository';
 import JobOffer from '../../models/db/JobOffer';
-import { httpClient } from '../../utils/httpClient'
-import { mapperJobOffer } from './utils'
+import { httpClient } from '../../utils/httpClient';
+import { mapperJobOffer } from './utils';
 import { JobOfferDto } from './utils';
 
 export const getById = async (id: string): Promise<JobOfferDto> => {
   const jobOffer = await findJobById(id);
-  if (isNullOrUndefined(jobOffer)) throw new HTTP404Error();
-  const host = process.env.CIENCIA_API_HOST || ''
-  const request = httpClient(host)
-  const response = await request.get(`/organizations/${jobOffer.organization_id}`)
-  jobOffer.organization = response
+  if (!jobOffer) throw notFoundError();
+  const host = process.env.CIENCIA_API_HOST || '';
+  const request = httpClient(host);
+  const response = await request.get(`/organizations/${jobOffer.organization_id}`);
+  jobOffer.organization = response;
   return mapperJobOffer(jobOffer);
 };
 
 export const saveJobOffer = async (jobOffer: JobOffer): Promise<number> => {
   //validar cosas dinamicas aca
-  const graph = await JobOffer.query()
-  .insert(jobOffer)
-  return graph.$id()
-
+  const graph = await JobOffer.query().insert(jobOffer);
+  return graph.$id();
 };
